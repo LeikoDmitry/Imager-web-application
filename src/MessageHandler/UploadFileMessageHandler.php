1ccdd8d99fd0705c0d6c7e425f8cd66c73586d19
Dmitry Leiko <dmitry.leiko@effective-soft.com>
2021-03-15 19:48:53 +0300

    Added PHP service for the messenger bus

--- src/MessageHandler/UploadFileMessageHandler.php
+++ src/MessageHandler/UploadFileMessageHandler.php
@@ -4,19 +4,23 @@ namespace App\MessageHandler;
 
 use App\Message\UploadFileMessage;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class UploadFileMessageHandler implements MessageHandlerInterface
 {
     private EntityManagerInterface $entityManager;
+    private LoggerInterface $logger;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function __invoke(UploadFileMessage $uploadFileMessage)
     {
         $context = $uploadFileMessage->getContext();
+        $this->logger->info(implode('|', $context));
     }
 }
